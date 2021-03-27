8496f5dbac7da52a2ec837790847a927e031fd81
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-27 23:27:37 +0900

    screenを追加

---
+++ lib/models/product.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  // ()と({})の違いはProduct()した時に引数を求められた時に、その変数が名前付きかの違いが生じる
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.price,
+      @required this.imageUrl,
+      this.isFavorite = false});
+}
