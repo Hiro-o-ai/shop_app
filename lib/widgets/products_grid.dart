b969453fb31e6f728b88e4ed11d6a03a7ca3454a
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-29 09:31:12 +0900

    providerによるstatemangmentを導入

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  // Providerを使用するので不要
+  // const ProductsGrid({
+  //   Key key,
+  //   @required this.loadedProducts,
+  // }) : super(key: key);
+
+  // final List<Product> loadedProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
+  }
+}
