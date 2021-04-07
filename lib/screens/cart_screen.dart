1fc198c541db9e94eba81e40f89810d7a2a4b37b
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-04-08 08:40:12 +0900

    cart内アイテムをスワイプで削除

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -64,6 +64,7 @@ class CartScreen extends StatelessWidget {
                 // cart.itemsはMapとして定義されているのでcart.items[i]ではできない
                 // だから一度リスト化する必要がある
                 cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
                 cart.items.values.toList()[i].title,
