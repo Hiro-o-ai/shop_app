c798f84f18465561a3304ef792e7b52b660fdaf0
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-04-20 23:54:45 +0900

    drawerにuser_product_screenを追加

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -17,11 +21,18 @@ class UserProductsScreen extends StatelessWidget {
             ),
           ],
         ),
+        drawer: AppDrawer(),
         body: Padding(
           padding: EdgeInsets.all(8),
           child: ListView.builder(
             itemCount: productsData.items.length,
-            itemBuilder: (_, i)=>,
+            itemBuilder: (_, i) => Column(
+              children: <Widget>[
+                UserProductItem(productsData.items[i].title,
+                    productsData.items[i].imageUrl),
+                Divider(),
+              ],
+            ),
           ),
         ));
   }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -10,6 +10,27 @@ class UserProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(title),
+      leading: CircleAvatar(
+        // image.network(url)はwidgetなのでfitなどができるが、NetworkImage(url)はwidgetではないのでできない
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {},
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {},
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
